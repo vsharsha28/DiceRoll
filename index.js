a31287fe514c68f255adb6cece57ca768ffee20e
Sai Harsha Vadde <vadde.sharsha.ece17@itbhu.ac.in>
2020-04-16 00:13:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+if(randomNumber2>randomNumber1){
+  document.querySelector("h1").textContent="Player 2 Wins!🚩";
+}
+else if(randomNumber2<randomNumber1){
+  document.querySelector("h1").textContent="🚩Player 1 Wins!";
+}
+else   document.querySelector("h1").textContent="It's a Draw!";
