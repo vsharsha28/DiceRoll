e2958228dad078168b99a94dabb94e66a10a7755
Sai Harsha Vadde <vadde.sharsha.ece17@itbhu.ac.in>
2020-04-16 00:14:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomNumber2=Math.floor(Math.random()*6)+1;
-document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+document.querySelector(".img1").setAttribute("src", "dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "dice"+randomNumber2+".png");
 if(randomNumber2>randomNumber1){
   document.querySelector("h1").textContent="Player 2 Wins!🚩";
 }
